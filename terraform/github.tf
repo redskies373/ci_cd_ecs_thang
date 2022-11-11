45b08bb5cbd66b1a1ba3022596dfedc007031a87
Brandy Butler <brandy@influencemobile.com>
2022-11-11 13:23:50 -0800

    updating a couple of things

--- terraform/github.tf
+++ terraform/github.tf
@@ -10,14 +10,8 @@ resource "github_actions_secret" "gha_secret_key" {
   plaintext_value  = aws_iam_access_key.gha-access-key.secret
 }
 
-resource "github_actions_secret" "gha_runner_subnet_id" {
-  repository       = var.gha_repo
-  secret_name      = "GHA_RUNNER_SUBNET_ID"
-  plaintext_value  = var.subnets[0]
-}
-
 resource "github_actions_secret" "gha_runner_sg" {
   repository       = var.gha_repo
   secret_name      = "GHA_RUNNER_SG"
-  plaintext_value  = aws_security_group.github-runner-sg.name
+  plaintext_value  = aws_security_group.github-runner-sg.id
 }
