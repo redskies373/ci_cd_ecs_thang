28bae8c46bde9e5f8a15fafb7992cb7d9ef09ee5
Brandy Butler <brandy@influencemobile.com>
2022-11-14 15:38:37 -0800

    trying to nail down policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,11 +8,37 @@ resource "aws_iam_policy" "gha-self-hosted-runner-policy" {
     Statement = [
       {
         Action = [
-            #"ec2:RunInstances",
-            #"ec2:TerminateInstances",
-            #"ec2:DescribeInstances",
-            #"ec2:DescribeInstanceStatus"
-            "*"
+          "ec2:RunInstances",
+          "ec2:TerminateInstances",
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus",
+          "ecr:DescribeImages",
+          "ecr:DescribeRepositories",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart",
+          "application-autoscaling:Describe*",
+          "application-autoscaling:PutScalingPolicy",
+          "application-autoscaling:RegisterScalableTarget",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:PutMetricAlarm",
+          "ecs:List*",
+          "ecs:Describe*",
+          "ecs:CreateService",
+          "elasticloadbalancing:Describe*",
+          "iam:AttachRolePolicy",
+          "iam:CreateRole",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:GetRole",
+          "iam:ListAttachedRolePolicies",
+          "iam:ListRoles",
+          "iam:ListGroups",
+          "iam:ListUsers"            
         ]
         Effect   = "Allow"
         Resource = "*"
