fba07365f3747ac2c2d25c9819dca9acebbaf31a
Brandy Butler <brandy@influencemobile.com>
2022-11-15 07:11:33 -0800

    parameratizing subnet into gh secrets

--- terraform/github.tf
+++ terraform/github.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "gha_runner_sg" {
   secret_name      = "GHA_RUNNER_SG"
   plaintext_value  = tostring(aws_security_group.github-runner-sg.id)
 }
+
+resource "github_actions_secret" "gha_runner_subnet" {
+  repository       = var.gha_repo
+  secret_name      = "GHA_RUNNER_SUBNET"
+  plaintext_value  = tostring(var.subnets[0])
+}
