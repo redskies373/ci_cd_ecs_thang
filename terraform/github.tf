20377174b7badd7228c88d99656ae38cb1bf2893
Brandy Butler <brandy@influencemobile.com>
2022-11-15 09:14:11 -0800

    changing to environment secrets, adding some security

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,11 +1,13 @@
-resource "github_actions_secret" "gha_runner_sg" {
+resource "github_actions_environment_secret" "gha_runner_sg" {
   repository       = var.gha_repo
+  environment      = "production"
   secret_name      = "GHA_RUNNER_SG"
   plaintext_value  = tostring(aws_security_group.github-runner-sg.id)
 }
 
-resource "github_actions_secret" "gha_runner_subnet" {
+resource "github_actions_environment_secret" "gha_runner_subnet" {
   repository       = var.gha_repo
+  environment      = "production"
   secret_name      = "GHA_RUNNER_SUBNET"
   plaintext_value  = tostring(var.subnets[0])
 }
