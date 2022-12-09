e1694a5952f1ef07447b4aef7bc96fa5e3673079
Brandy Butler <brandy@influencemobile.com>
2022-12-09 07:30:16 -0800

    simulating change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World! <h1>HELLO NURSE!</h1>"
+    return "Hello, World! <h1>Hello IM Devops!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
